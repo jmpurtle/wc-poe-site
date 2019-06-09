c390c2911b4d842db9f10166f9f6ea94e700b604
John Purtle <purtlejohnmark@gmail.com>
2019-06-09 12:33:07 -0500

    Migrated for sake of module name

---
+++ web/app/wc_poe_site/root.py
@@ -0,0 +1,14 @@
+# HTTP status code exception for "302 Found" redirection.
+from webob.exc import HTTPFound
+
+
+class GameAdvert:
+	"""Main game advertisement page."""
+
+	def __init__(self, context):
+		"""Executed when the root of the site (or children) are accessed, on each request."""
+		self._ctx = context # Store the "request context" for later use.
+
+	def __call__(self):
+		"""Called to handle direct requests to the web root itself."""
+		return HTTPFound(location='/Game') # Issue the redirect.
