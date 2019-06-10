8b1f5c9e7e91a20750753e1e4991fc5f5e1ef603
Alice Bevan-McGregor <alice@gothcandy.com>
2019-06-10 15:27:35 -0400

    Python 3 namespace packaging changes, updated pinned versions.

--- setup.py
+++ setup.py
@@ -21,17 +21,12 @@ setup(
 	license = "mit",
 	keywords = [],
 
-	packages = find_packages(exclude=['test', 'example', 'conf', 'benchmark', 'tool', 'doc']),
+	packages = ('web.app.wc_poe_site', ),
 	include_package_data = True,
 	package_data = {'': [
 		'README.rst',
 		'LICENSE.txt'
 	]},
-
-	namespace_packages = [
-		'web', # primary namespace
-		'web.app', # application code goes here
-	],
 
 	setup_requires = [
 		'pytest-runner',
@@ -48,11 +43,11 @@ setup(
 	],
 
 	install_requires = [
-		'WebCore[development]>=2.0,<3', # Web framework.
-		'marrow.mongo', # Database connectivity.
-		'web.dispatch.object', # Object (class-based filesystem-like) dispatch.
-		'web.dispatch.resource', # Resource (RESTful) dispatch.
-		'cinje', # Template engine.
+		'WebCore[development]~=3.0', # Web framework.
+		'web.dispatch.object~=3.0', # Object (class-based filesystem-like) dispatch.
+		'web.dispatch.resource~=3.0', # Resource (RESTful) dispatch.
+		'marrow.mongo~=2.0', # Database connectivity.
+		'cinje~=1.1.2', # Template engine.
 	],
 
 	extras_require = dict(
