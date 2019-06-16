b7b3700098ba9b751696dbf4d2b02104eae636bf
John Purtle <purtlejohnmark@gmail.com>
2019-06-16 10:24:09 -0500

    Resolved variable name conflicts

--- web/app/wc_poe_site/template.py
+++ web/app/wc_poe_site/template.py
@@ -2,14 +2,14 @@
 
 : from cinje.std.html import page
 
-:def render context, sitepage, page
-: using page page['_id']
+: def render context, sitepage, pageData
+: using page pageData['_id']
 : classes = {'SitePage'}
 
-: if not page['content']
+: if not pageData['content']
 	: classes.add('placeholder')
 : end
 
 <main&{class_=classes}>
-	#{page['content'] or "<i>No content.</i>"}
+	#{pageData['content'] or "<i>No content.</i>"}
 </main>
